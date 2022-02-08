f93d30d3905004943db83e95bbb59618d89c651f
maribelcuales <maribelcuales@gmail.com>
2022-02-08 21:57:29 +0800

    Selecting all columns.

--- mode-sql.sql
+++ mode-sql.sql
@@ -1,6 +1,15 @@
 SELECT * FROM tutorial.us_housing_units
 
 SELECT  year,
+        month,
+        month_name,
+        south,
+        west,
+        midwest,
+        northeast
+  FROM tutorial.us_housing_units
+
+SELECT  year,
         month, 
         west 
   FROM tutorial.us_housing_units
