e09571b15c492f1d01fa4a37533fb8a76cee1ab9
maribelcuales <maribelcuales@gmail.com>
2022-02-22 21:38:09 +0800

    Query that shows all top songs from January 1, 1985 through December 31, 1990.

--- mode-sql.sql
+++ mode-sql.sql
@@ -269,3 +269,8 @@ SELECT *
 SELECT *
   FROM tutorial.billboard_top_100_year_end
  WHERE year_rank >= 5 AND year_rank <= 10 
+
+/* Query that shows all top songs from January 1, 1985 through December 31, 1990.*/
+SELECT * 
+  FROM tutorial.billboard_top_100_year_end
+ WHERE year BETWEEN 1985 AND 1990
