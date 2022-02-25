007d349ceb84462565ae451d2d03954371c5ad3c
maribelcuales <maribelcuales@gmail.com>
2022-02-25 08:14:56 +0800

    Query for all songs in the 1960s with love in the title.

--- mode-sql.sql
+++ mode-sql.sql
@@ -324,3 +324,10 @@ SELECT *
   FROM tutorial.billboard_top_100_year_end
  WHERE year_rank = 1
    AND year IN (1990, 2000, 2010)
+
+/* Write a query that lists all songs from the 1960s with "love" in the title. */
+SELECT * 
+  FROM tutorial.billboard_top_100_year_end
+ WHERE year BETWEEN 1960 AND 1969
+   AND song_name ILIKE '%love%'
+
