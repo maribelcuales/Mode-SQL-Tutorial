f45daa68c86ef4586be567d9565c2b77929b37f5
maribelcuales <maribelcuales@gmail.com>
2022-02-25 08:14:08 +0800

    Query for top 1 songs in 1990, 2000, and 2010.

--- mode-sql.sql
+++ mode-sql.sql
@@ -318,3 +318,9 @@ SELECT *
   FROM tutorial.billboard_top_100_year_end
  WHERE year_rank <= 10
    AND "group" ILIKE '%Ludacris%'
+
+/* Write a query that surfaces the top-ranked records in 1990, 2000, and 2010. */
+SELECT * 
+  FROM tutorial.billboard_top_100_year_end
+ WHERE year_rank = 1
+   AND year IN (1990, 2000, 2010)
