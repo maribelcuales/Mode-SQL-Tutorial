e2b31cf77d6b996c173ef5d7774f90e8ab92ed18
maribelcuales <maribelcuales@gmail.com>
2022-02-26 10:35:08 +0800

    Query combining AND with OR.

--- mode-sql.sql
+++ mode-sql.sql
@@ -333,7 +333,9 @@ SELECT *
 
 /* 
 SQL OR
-OR is a logical operator in SQL that allows you to select rows that satisfy either of two conditions. It works the same way as AND, which selects the rows that satisfy both of two conditions.
+OR is a logical operator in SQL that allows you to select rows that satisfy either of two conditions. 
+
+It works the same way as AND, which selects the rows that satisfy both of two conditions.
 */
 
 /*Query for rows where year_rank is 5 OR artist is 'Gotye'.*/
@@ -345,4 +347,10 @@ SELECT *
 SELECT *
   FROM tutorial.billboard_top_100_year_end
  WHERE year_rank = 5 OR "group" ILIKE '%feat%'
+
+/* You can combine AND with OR using parenthesis. Query will return rows that satisfy both of the following conditions. */
+SELECT *
+  FROM tutorial.billboard_top_100_year_end
+ WHERE year = 2013
+   AND ("group" ILIKE '%macklemore%' OR "group" ILIKE '%timberlake%')
 
