fff092ab6ddc2aa4f1ff3d742f297ecec97bc53f
maribelcuales <maribelcuales@gmail.com>
2022-02-27 17:22:54 +0800

    Using NOT with > and < will result to an error.

--- mode-sql.sql
+++ mode-sql.sql
@@ -393,3 +393,9 @@ SELECT *
  WHERE year = 2013
    AND year_rank NOT BETWEEN 2 AND 3
 
+/* NOT with > and < does not make sense and will return an error */
+SELECT *
+  FROM tutorial.billboard_top_100_year_end 
+ WHERE year = 2013
+   AND year_rank NOT > 3
+
