53d19f4026577626edf68e1d3553286785882d76
maribelcuales <maribelcuales@gmail.com>
2022-04-29 22:30:29 +0800

    Added ORDER BY year in descending order.

--- mode-sql.sql
+++ mode-sql.sql
@@ -454,3 +454,10 @@ SELECT *
   FROM tutorial.billboard_top_100_year_end
  WHERE year = 2013
  ORDER BY year_rank
+
+/* ORDER BY results in opposite order (descending) */ 
+SELECT *
+  FROM tutorial.billboard_top_100_year_end
+ WHERE year = 2013
+ ORDER BY year_rank DESC
+ 
