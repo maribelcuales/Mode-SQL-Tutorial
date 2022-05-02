f75f4ab09b658b685128193f1ab37b84b3aba6ff
maribelcuales <maribelcuales@gmail.com>
2022-05-02 21:25:32 +0800

    Added notes on SQL Aggregate functions.

---
+++ intermediate-sql.sql
@@ -0,0 +1,13 @@
+/* The Apple Stock Prices Dataset */
+SELECT * 
+  FROM tutorial.aapl_historical_stock_price
+
+/* SQL AGGREGATE FUNCTIONS 
+SQL is excellent at aggregating data the way you might in a pivot table in Excel.
+
+COUNT - counts how many rows are in a particular column.
+SUM - adds together all the values in a particular column.
+MIN and MAX - return the lowest and highest values in a particular column, respectively.
+AVG - calculates the average of a group of selected values.
+*/
+
