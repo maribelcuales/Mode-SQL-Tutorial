211561769bdeadc3fc52e70679e88fd3a81dbe50
maribelcuales <maribelcuales@gmail.com>
2022-05-04 22:18:57 +0800

    Query for Apple's lowest stock price.

--- intermediate-sql.sql
+++ intermediate-sql.sql
@@ -78,3 +78,7 @@ SELECT MIN(volume) AS min_volume,
        MAX(volume) AS max_volume
   FROM tutorial.aapl_historical_stock_price
 
+-- Query for Apple's lowest stock price 
+SELECT MIN(low)
+  FROM tutorial.aapl_historical_stock_price
+
