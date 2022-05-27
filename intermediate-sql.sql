03d023ec344b1d0355a341994ff5b58381739029
maribelcuales <maribelcuales@gmail.com>
2022-05-27 21:17:17 +0800

    Rewrite query so CASE statements do not overlap.

--- intermediate-sql.sql
+++ intermediate-sql.sql
@@ -262,3 +262,15 @@ SELECT player_name,
             WHEN weight > 175 THEN '176-200'
             ELSE '175 or under' END AS weight_group
   FROM benn.college_football_players
+
+-- BEST PRACTICE: Create statements that don't overlap 
+SELECT player_name,
+       weight,
+       CASE WHEN weight > 250 THEN 'over 250'
+            WHEN weight > 200 AND weight <= 250 THEN '201-250'
+            WHEN weight > 175 AND weight <= 200 THEN '176-200'
+            ELSE '175 or under' END AS weight_group
+  FROM benn.college_football_players
+
+
+
