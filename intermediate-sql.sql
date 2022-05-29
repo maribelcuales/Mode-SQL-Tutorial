9c61fc4ba8f09ad312e0545a221353a64edbe58a
maribelcuales <maribelcuales@gmail.com>
2022-05-29 20:26:28 +0800

    Counting multiple conditions in one query.

--- intermediate-sql.sql
+++ intermediate-sql.sql
@@ -322,3 +322,13 @@ SELECT COUNT(1) AS fr_count
   FROM benn.college_football_players
  WHERE year = 'FR'
 
+-- Counting multiple conditions in one query (The WHERE clause only allows you to count one condition)
+SELECT CASE WHEN year = 'FR' THEN 'FR'
+            WHEN year = 'SO' THEN 'SO'
+            WHEN year = 'JR' THEN 'JR'
+            WHEN year = 'SR' THEN 'SR'
+            ELSE 'No Year Data' END AS year_group,
+            COUNT(1) AS count
+  FROM benn.college_football_players
+ GROUP BY 1
+
