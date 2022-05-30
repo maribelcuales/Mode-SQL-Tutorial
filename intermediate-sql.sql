aa3346c3156d531cddbac4d60337fb28aab22577
maribelcuales <maribelcuales@gmail.com>
2022-05-30 20:05:52 +0800

    Query with repeat entire CASE statement.

--- intermediate-sql.sql
+++ intermediate-sql.sql
@@ -344,3 +344,18 @@ SELECT CASE WHEN year = 'FR' THEN 'FR'
   FROM benn.college_football_players
  GROUP BY year_group
 
+-- if you do choose to repeat the entire CASE statement, you should remove the AS year_group column naming when you copy/paste into the GROUP BY clause:
+SELECT CASE WHEN year = 'FR' THEN 'FR'
+            WHEN year = 'SO' THEN 'SO'
+            WHEN year = 'JR' THEN 'JR'
+            WHEN year = 'SR' THEN 'SR'
+            ELSE 'No Year Data' END AS year_group,
+            COUNT(1) AS count
+  FROM benn.college_football_players
+ GROUP BY CASE WHEN year = 'FR' THEN 'FR'
+               WHEN year = 'SO' THEN 'SO'
+               WHEN year = 'JR' THEN 'JR'
+               WHEN year = 'SR' THEN 'SR'
+               ELSE 'No Year Data' END
+
+               
