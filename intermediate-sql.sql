5dd9c667e9feda1e69e47e80339a0a1b0748fc32
maribelcuales <maribelcuales@gmail.com>
2022-06-20 22:53:41 +0800

    Added notes on SQL OUTER JOINS.

--- intermediate-sql.sql
+++ intermediate-sql.sql
@@ -583,4 +583,14 @@ SELECT players.player_name,
     ON teams.school_name = players.school_name
  WHERE teams.division = 'FBS (Division I-A Teams)'
 
- 
+
+/* 
+SQL OUTER JOINS 
+When performing an inner join, rows from either table that are unmatched in the other table are not returned. 
+
+In an outer join, unmatched rows in one or both tables can be returned. There are a few types of outer joins:
+
+LEFT JOIN - returns only unmatched rows from the left table.
+RIGHT JOIN - returns only unmatched rows from the right table.
+FULL OUTER JOIN - returns unmatched rows from both tables.
+*/
